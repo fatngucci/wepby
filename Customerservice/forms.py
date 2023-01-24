7ee523283343fe79922720d4a60d03f32a7aa583
s85524 <s85524@bht-berlin.de>
2023-01-25 00:36:18 +0100

    Signup form crispy

--- Customerservice/forms.py
+++ Customerservice/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Column, Div, Submit, Row, Button
 from django import forms
 from Snacks.models import Snack, Comment
 
@@ -9,6 +11,40 @@ class SnackEditForm(forms.ModelForm):
         widgets = {
             'snack_id': forms.HiddenInput(),
         }
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Row(
+                Column('name', css_class='form-group  mx-auto'),
+                Column('gewicht', css_class='form-group mx-auto'),
+            ),
+            Row(
+
+                Column('beschreibung', css_class='form-group  mx-auto'),
+            ),
+            Row(
+                Column('artikelnummer', css_class='form-group  mx-auto'),
+                Column('preis', css_class='form-group mx-auto'),
+
+            ),
+            Row(
+
+                Column('bilder', css_class='form-group  mx-auto'),
+            ),
+            Row(
+
+                Column('produkt_info', css_class='form-group mx-auto'),
+            ),
+            Div(
+                Submit('submit', 'Edit', css_class='btn mx-auto'),
+                Submit('cancel', 'Cancel', css_class='btn mx-auto'),
+                css_class='text-center'
+            ),
+        )
+    """
 
 
 class CommentEditForm(forms.ModelForm):
--- Useradmin/forms.py
+++ Useradmin/forms.py
@@ -1,4 +1,7 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Column, Row, Div, Submit
 from django.contrib.auth.forms import UserCreationForm
+
 from .models import MyUser
 
 
@@ -6,6 +9,33 @@ class MySignUpForm(UserCreationForm):
     class Meta:
         model = MyUser
         fields = (
-        'username', 'first_name', 'last_name', 'email', 'date_of_birth', 'profile_picture', 'some_file', 'password1',
-        'password2')
+            'username', 'first_name', 'last_name', 'email', 'date_of_birth', 'profile_picture', 'some_file', 'password1',
+            'password2')
     # password ist wegen UserCreationForm schon mit dabei
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('first_name', css_class='form-group mx-1'),
+                Column('last_name', css_class='form-group mx-1'),
+            ),
+            Row(
+                Column('username', css_class='form-group mx-1'),
+                Column('email', css_class='form-group mx-1'),
+            ),
+            Row(
+                Column('date_of_birth', css_class='form-group mx-1'),
+                Column('profile_picture', css_class='form-group mx-1'),
+                Column('some_file', css_class='form-group mx-1'),
+            ),
+            Row(
+                Column('password1', css_class='form-group mx-1'),
+                Column('password2', css_class='form-group mx-1'),
+            ),
+            Div(
+                Submit('submit', 'Signup', css_class='btn mx-auto'),
+                css_class='text-center mb-5'
+            )
+        )
